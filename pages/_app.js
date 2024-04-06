34ba1a53594cd82e53e8c6e93f99789d6f3a7c19
osuji chiaka <chiakajunior@yahoo.com>
2024-04-06 06:38:29 +0300

    worked on the page loader

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,29 @@ import { useRouter } from 'next/router';
 import {State} from "../context/context"
 import '../styles/globals.css';
 
+function LoadingNewPage() {
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const handleStart = (url) => url !== router.asPath && setLoading(true);
+    const handleComplete = (url) =>  setLoading(false);
+    router.events.on("routeChangeStart", handleStart);
+    router.events.on("routeChangeComplete", handleComplete);
+    router.events.on("routeChangeError", handleComplete);
+    return () => {
+      router.events.off("routeChangeStart", handleStart);
+      router.events.off("routeChangeComplete", handleComplete);
+      router.events.off("routeChangeError", handleComplete);
+    };
+  });
+  return (
+    loading && (
+      <div className={loading ? "pageLoader" : "pageLoaderInactive"}></div>
+    )
+  );
+}
+
 
 function MyApp({ Component, pageProps }) {
   return( 
@@ -16,6 +39,7 @@ function MyApp({ Component, pageProps }) {
       </Head>
     <State>
     <div className='font-primary'>
+    <LoadingNewPage />
     <Component {...pageProps} />
     </div>
     </State>
