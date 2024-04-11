54018a6945226362f21381be49f74fb6d191cdb4
osuji chiaka <chiakajunior@yahoo.com>
2024-04-11 19:40:01 +0300

    working

--- pages/index.js
+++ pages/index.js
@@ -90,18 +90,18 @@ export default function Home() {
   
     <div className="mt-[4%] max-large:mt-[22%] w-full h-[90vh] max-large:h-[300px] relative">
       <div className="animate__animated animate__slower animate__slideInLeft absolute top-[10%] 2xl:top-[15%] max-large:top-[3%] left-[10%] 2xl:left-[15%] max-large:left-[5%] w-[45%] max-large:w-[90%] 2xl:w-[35%] z-30">  
-        <header className={`font-header text-[3em] 2xl:text-[3.2em] max-large:text-[1.5rem] text-btn_color absolute top-[4%] left-0 ${toggleIdentity ? "header1" : "header2"}`}>
+        <header className={`font-header text-[3em] 2xl:text-[3.5em] max-large:text-[1.5rem] text-btn_color absolute top-[4%] left-0 ${toggleIdentity ? "header1" : "header2"}`}>
           Nurturing Young Talents from Slum Communities in Africa Into Tech
           </header>
           
-          <header className={`font-header text-[3em] 2xl:text-[3.2em] max-large:text-[1.5rem] text-btn_color absolute top-[4%] left-0 ${toggleIdentity ? "header2" : "header1"}`}>
+          <header className={`font-header text-[3em] 2xl:text-[3.5em] max-large:text-[1.5rem] text-btn_color absolute top-[4%] left-0 ${toggleIdentity ? "header2" : "header1"}`}>
           Let's Inspire and support the Next Generation of Tech-Savy Innovators as they Shape 
           the World of Tommorrow!
           </header>        
       </div>
 
       <div className="animate__animated animate__slower animate__slideInLeft z-30 absolute top-[60%] 2xl:top-[45%] max-large:top-[52%] left-[15%] max-large:left-[5%]">
-      <div className="join font-normal text-normal 2xl:text-[1.2em] max-large:text-[12px] max-large:h-[310px] text-btn_color w-[40%] max-large:w-[70%] 2xl:w-[28%]">
+      <div className="join font-normal text-normal 2xl:text-[1.2em] max-large:text-[12px] max-large:h-[310px] text-btn_color w-[40%] max-large:w-[70%] 2xl:w-[35%]">
         Join us in our mission to empower Africa's youth through technology
         Together, we can build a more inclusive and empowered Africa
       </div>
@@ -140,7 +140,7 @@ export default function Home() {
           </div>
           </div>
         </div> */}
-        <div className="flex flex-row absolute bottom-[15%] right-[15%] text-btn_color">
+        <div className="flex flex-row absolute bottom-[5%] right-[5%] text-btn_color">
                 <div className="w-[15.625rem] max-large:w-full max-large:mt-[5%]">
                     <h2 className="flex flex-row">
                     <div className="relative w-[1rem] h-[1.25rem] mr-2">
@@ -157,7 +157,7 @@ export default function Home() {
                     </p>
                 </div>
 
-                <div className="ml-5">
+                <div className="ml-6">
                     <h2 className="flex flex-row">
                     <div className="relative w-[40px] h-[26px] mr-2">
                         <Image 
