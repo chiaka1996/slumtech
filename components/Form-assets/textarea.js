66b25ea6144d584da96883ff27a5da916571551e
osuji chiaka <chiakajunior@yahoo.com>
2024-04-22 16:48:40 +0300

    removed border on text-area

--- components/Form-assets/textarea.js
+++ components/Form-assets/textarea.js
@@ -7,7 +7,7 @@ const TextAreaComponent = ({labelName, placeholder, onChangeInput, name, value})
             name={name}
             value={value}
             onChange={onChangeInput}
-            className="mt-[1%] rounded-[0.5rem] px-2 outline-none border border-subtext w-[100%] h-[7.5rem] text-[#999999] font-normal text-twelve xlarge:text-normal placeholder-[#999999] ">
+            className="mt-[1%] rounded-[0.5rem] px-2 outline-none border border-subtext border-x-0 border-t-0 border-b-1 w-[100%] h-[7.5rem] text-[#999999] font-normal text-twelve xlarge:text-normal placeholder-[#999999] ">
 
             </textarea>
         </div>
